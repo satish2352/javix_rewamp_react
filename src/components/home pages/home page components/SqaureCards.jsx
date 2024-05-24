8da687fd82dfd5c6286fdf5a66140ee58c93b543
DhanuPatil0654 <dhananjaypatil572001@gmail.com>
2024-05-24 16:53:54 +0530

    minor changes

--- src/components/home pages/home page components/SqaureCards.jsx
+++ src/components/home pages/home page components/SqaureCards.jsx
@@ -4,10 +4,10 @@ import './SquareCard.css'
 const SquareCards = ({ cardsData }) => {
     return (
         <>
-            <Row >
+            <Row className='justify-content-center'>
                 {cardsData.map((card, index) => (
                     <Col key={index} lg={3} className='mt-3'>
-                        <Card id='Homepage_card'>
+                        <Card id='Homepage_card' className='border-0 shadow-sm'>
                             <Card.Body className='Homepage_card'>
                                 <Card.Title style={{ textAlign: " center" }}>{card.title}</Card.Title>
                                 <Card.Text style={{ textAlign: " center" }}>{card.text}</Card.Text>
