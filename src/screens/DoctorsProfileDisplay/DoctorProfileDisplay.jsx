e9d932d6f717453598b5de534bfa865744238e36
sahil0709 <a.sahil0709@gmail.com>
2024-06-06 13:31:24 +0530

    Doctor Profile Modification

--- src/screens/DoctorsProfileDisplay/DoctorProfileDisplay.jsx
+++ src/screens/DoctorsProfileDisplay/DoctorProfileDisplay.jsx
@@ -7,20 +7,27 @@ import doctorphoto from '../DoctorsProfileDisplay/photodoctor.png'
 import doctorsign from '../DoctorsProfileDisplay/doctorsign.png'
 function DoctorProfileDisplay() {
     const navigate = useNavigate()
-    const profileInfo = [
+
+    const personalInfo = [
         { label: "First Name", value: "John" },
         { label: "Last Name", value: "Smith" },
         { label: "Gender", value: "Male" },
         { label: "Date of Birth", value: "01/01/2001" },
         { label: "Email", value: "johnsmith@gmail.com" },
-        { label: "Mobile Number", value: "9876543210" },
-        { label: "State", value: "California" },
-        { label: "District", value: "Los Angeles" },
+        { label: "Mobile Number", value: "9876543210" }
+    ]
+
+    const medicalInfo = [
         { label: "Qualification", value: "M.B.B.S" },
         { label: "Medical Registration Number", value: "12658343920" },
         { label: "Registration Year", value: "2009" },
         { label: "Experience (in years)", value: "10" },
-        { label: "Specialization", value: "Cardiology" },
+        { label: "Specialization", value: "Cardiology" }
+    ]
+
+    const addressInfo = [
+        { label: "State", value: "California" },
+        { label: "District", value: "Los Angeles" },
         { label: "Country", value: "USA" },
         { label: "Address", value: "8358, Sunset Boulevard" },
         { label: "Pin Code", value: "90069" },
@@ -39,44 +46,68 @@ function DoctorProfileDisplay() {
             <Home />
             <br></br>
             <Card>
+                <Card.Header>Doctor Profile</Card.Header>
                 <Card.Body>
-                    <div className="profile-title">Doctor Profile</div>
+                    {/* <div className="profile-title">Doctor Profile</div> */}
                     <div className="profile-fields">
                         <Row className="field d-flex">
-                            <Col lg={3} md={6} sm={12}><span>Photo:</span></Col>
-                            <Col lg={3} md={6} sm={12}><img src={doctorphoto} alt="Doctor's Photo" width={"150px"}/></Col>
-                            <Col lg={3} md={6} sm={12}><span>Signature:</span></Col>
-                            <Col lg={3} md={6} sm={12}><img src={doctorsign} alt="Doctor's Signature"  width={"120px"}/></Col>
-                        </Row>
-                        <Row className='mt-3'>
-                            <Col lg={6} md={6} sm={12}>
-                                {profileInfo.slice(0, 8).map((field, index) => (
-                                    <Row className="field" key={index}>
-                                        <Col md={6}><span>{field.label}:</span></Col>
-                                        <Col md={6}><p>{field.value}</p></Col>
-                                    </Row>
-                                ))}
-                            </Col>
-                            <Col lg={6} md={6} sm={12}>
-                                {profileInfo.slice(8).map((field, index) => (
-                                    <Row className="field" key={index + 8}>
-                                        <Col md={6}><span>{field.label}:</span></Col>
-                                        <Col md={6}><p>{field.value}</p></Col>
-                                    </Row>
-                                ))}
-                            </Col>
-                            <Col lg={6} md={6} sm={6}>
-                                <br></br>
-                                <Button variant="primary" onClick={() => editdoctor()}>Edit Profile</Button>
-                            </Col>
-                            <Col lg={6} md={6} sm={6}>
-                                <br></br>
-                                <Button  variant="primary" onClick={() => changepassworddoctor()} >Change Password</Button>
-                            </Col>
+                            <Col lg={2} md={6} sm={12}><span>Photo:</span></Col>
+                            <Col lg={4} md={6} sm={12}><img src={doctorphoto} alt="Doctor's Photo" width={"150px"} /></Col>
+                            <Col lg={2} md={6} sm={12}><span>Signature:</span></Col>
+                            <Col lg={4} md={6} sm={12}><img src={doctorsign} alt="Doctor's Signature" width={"120px"} /></Col>
                         </Row>
                     </div>
                 </Card.Body>
             </Card>
+            <Card className='mt-4'>
+                <Card.Header>Personal Information</Card.Header>
+                <Card.Body>
+                    <div>
+                        <Row className='mt-3'>
+                            {personalInfo.map((field, index) => (
+                                <Col lg={4} md={6} sm={12} className="field mt-4" key={index}>
+                                    <span>{field.label}:</span>
+                                    <p className='mt-1'>{field.value}</p>
+                                </Col>
+                            ))}
+                        </Row>
+                    </div>
+                </Card.Body >
+            </Card >
+            <Card className='mt-4'>
+                <Card.Header>Medical Information</Card.Header>
+                <Card.Body>
+                    <div>
+                        <Row className='mt-3'>
+                            {medicalInfo.map((field, index) => (
+                                <Col lg={4} md={6} sm={12} className="field mt-4" key={index}>
+                                    <span>{field.label}:</span>
+                                    <p className='mt-1'>{field.value}</p>
+                                </Col>
+                            ))}
+                        </Row>
+                    </div>
+                </Card.Body >
+            </Card >
+            <Card className='mt-4'>
+                <Card.Header>Address Information</Card.Header>
+                <Card.Body>
+                    <div>
+                        <Row className='mt-3'>
+                            {addressInfo.map((field, index) => (
+                                <Col lg={4} md={6} sm={12} className="field mt-4" key={index}>
+                                    <span>{field.label}:</span>
+                                    <p className='mt-1'>{field.value}</p>
+                                </Col>
+                            ))}
+                        </Row>
+                    </div>
+                </Card.Body >
+            </Card >
+            <div className='d-flex gap-2 mt-4'>
+                <Button variant="primary" onClick={() => editdoctor()}>Edit Profile</Button>
+                <Button variant="primary" onClick={() => changepassworddoctor()} >Change Password</Button>
+            </div>
         </>
     )
 }
